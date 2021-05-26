8ed3caa5f86a2b61db75ca00a87ba5d9336289ba
Anoob-21 <an.80.2021@gmail.com>
2021-05-26 09:30:01 +0530

    Added TC5 and 6

---
+++ src/TestClass5.java
@@ -0,0 +1,5 @@
+public class TestClass5 {
+    private int empid;
+    private int empno;
+    private int empDEPT;
+}
---
+++ src/TestClass6.java
@@ -0,0 +1,5 @@
+public class TestClass6 {
+    private int empid;
+    private int empno;
+    private int empDEPT;
+}
